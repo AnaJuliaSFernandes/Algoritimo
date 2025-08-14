e1717285720738694f34f0404c7e071aea95f359
Ana Julia <131393446+AnaJuliaSFernandes@users.noreply.github.com>
2025-08-13 21:39:16 -0300

    13/08

---
+++ exercicios.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    /*double angulo, seno, cosseno, tangente;
+    printf("Informe o valor do angulo: ");
+    scanf("%lf", &angulo);
+    seno = sin(angulo);
+    cosseno = cos(angulo);
+    tangente = tan(angulo);
+    printf("Seno: %.2lf\n", seno);
+    printf("Cosseno: %.2lf\n", cosseno);
+    printf("Tangente: %.2lf\n", tangente);*/
+
+    /*double num, raiz;
+    printf("Informe o numero:");
+    scanf("%lf", &num);
+    raiz = sqrt(num);
+    printf("A raiz do numero eh: %.2lf\n", raiz);*/
+
+    /*double x, y, resultado;
+    printf("Informe o valor de x:");
+    scanf("%lf", &x);
+    printf("Informe o valor de y: ");
+    scanf("%lf", &y);
+    resultado = pow(x, y);
+    printf("Resultado: %.2lf\n", resultado);*/
+
+    /*double num;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    printf("Floor: %lf\n", floor(num));
+    printf("Ceil: %lf\n", ceil(num));
+    printf("Round: %lf\n", round(num));*/
+
+    /*ex1
+    double num, quadrado, cubo;
+    printf("Informe um numero:");
+    scanf("%lf", &num);
+    quadrado = pow(num,2);
+    cubo = pow(num,3);
+    printf("O numero ao quadrado eh: %lf\n", quadrado);
+    printf("O numero ao cubo eh: %lf\n", cubo);*/
+
+    /*ex2*/
+    double num1, num2, raiz;
+    printf("Informe o primeiro numero:");
+    scanf("%lf", &num1);
+    printf("Informe o segundo numero:");
+    scanf("%lf", &num2);
+    raiz = sqrt(num1*num2);
+    printf("O numero tranformado eh: %.2lf\n", raiz);
+
+    return 0;
+}
