9cc58a63ccde6ed31497353fc14c9d35cfba112c
Ana Julia <131393446+AnaJuliaSFernandes@users.noreply.github.com>
2025-09-19 00:24:56 -0300

    Update ex_prova.c

--- ex_prova.c
+++ ex_prova.c
@@ -9,7 +9,7 @@ int main()
 
     switch(codigo){
         case 1:{
-            printf("Alimento n�o-perecivel");
+            printf("Alimento não-perecivel");
             break;
         }
         case 2:{
@@ -109,3 +109,74 @@ int main()
 
     return 0;
 }
+    /*int voto;
+    int cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0;
+    int nulo = 0, branco = 0;
+
+    printf("Digite os votos (1 a 6). Digite 0 para encerrar:\n");
+
+    do {
+        scanf("%d", &voto);
+
+        switch (voto) {
+            case 1: cand1++; break;
+            case 2: cand2++; break;
+            case 3: cand3++; break;
+            case 4: cand4++; break;
+            case 5: nulo++; break;
+            case 6: branco++; break;
+            case 0: break; // finaliza
+            default: 
+                printf("Código inválido! Digite de novo.\n");
+        }
+
+    } while (voto != 0);
+
+    printf("\nRESULTADO DA ELEIÇÃO:\n");
+    printf("Candidato 1: %d votos\n", cand1);
+    printf("Candidato 2: %d votos\n", cand2);
+    printf("Candidato 3: %d votos\n", cand3);
+    printf("Candidato 4: %d votos\n", cand4);
+    printf("Nulos: %d votos\n", nulo);
+    printf("Brancos: %d votos\n", branco);
+
+    return 0;
+}*/
+
+/*int valor;
+    int n50, n10, n5, n1;
+
+    printf("Digite o valor para saque (0 encerra):\n");
+
+    while (1) {
+        scanf("%d", &valor);
+
+        if (valor == 0) {
+            break; // finaliza o programa
+        }
+
+        // Cálculo das notas
+        n50 = valor / 50;
+        valor = valor % 50;
+
+        n10 = valor / 10;
+        valor = valor % 10;
+
+        n5 = valor / 5;
+        valor = valor % 5;
+
+        n1 = valor;
+
+        // Resultado
+        printf("Notas de 50: %d\n", n50);
+        printf("Notas de 10: %d\n", n10);
+        printf("Notas de 5 : %d\n", n5);
+        printf("Notas de 1 : %d\n\n", n1);
+
+        printf("Digite outro valor (0 encerra):\n");
+    }
+
+    printf("Programa encerrado.\n");
+    return 0;
+}*/
+
